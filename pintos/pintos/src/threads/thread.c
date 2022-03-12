dbf778f8ead52560309ea93a6f8a119654396c19
Peyman Haji Mohamad <peyman.eb15@gmail.com>
2022-03-13 00:25:38 +0330

    Fix a minor bug in calling thread_create() in thread.c

--- pintos/pintos/src/threads/thread.c
+++ pintos/pintos/src/threads/thread.c
@@ -108,7 +108,7 @@ thread_start (void)
   /* Create the idle thread. */
   struct semaphore idle_started;
   sema_init (&idle_started, 0);
-  thread_create ("idle", PRI_MIN, idle, &idle_started);
+  thread_create ("idle", PRI_MIN, idle, &idle_started,NULL);
 
   /* Start preemptive thread scheduling. */
   intr_enable ();
