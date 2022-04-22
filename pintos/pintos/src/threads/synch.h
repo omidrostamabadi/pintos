86116d731116bf29f26c832b71b097321ff8e462
Omid Rostamabadi <omidrostamabadi@gmail.com>
2022-04-22 23:43:09 +0430

    Add waiters_pq for condition struct

--- pintos/pintos/src/threads/synch.h
+++ pintos/pintos/src/threads/synch.h
@@ -33,7 +33,7 @@ bool lock_held_by_current_thread (const struct lock *);
 /* Condition variable. */
 struct condition
   {
-    struct list waiters;        /* List of waiting threads. */
+    struct thread_node *waiters_pq;        /* List of waiting threads. */
   };
 
 void cond_init (struct condition *);
